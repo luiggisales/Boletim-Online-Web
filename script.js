c3cd248073b08fe14c83d05983d37434fa8cea94
luiggisales <luiggi.sales.silva07@aluno.ifce.edu.br>
2022-04-01 16:18:15 -0300

    subindo dnv

--- script.js
+++ script.js
@@ -2,11 +2,25 @@ function Login(event) {
     event.preventDefault(); // Cancela o evento se for cancelável, sem parar a propagação do mesmo.
     var matricula = document.getElementsByName('matricula')[0].value;
     var senha = document.getElementsByName('senha')[0].value;
-  
+
     if (matricula == "202001" && senha == "1234") {
       alert("Dados corretos");
       window.location.href= "./area-estudante/index.html";
     }else{
       alert("Dados incorretos, tente novamente");
     }
+}
+
+
+function LoginProfessor(event) {
+  event.preventDefault()
+  var matricula_professor = document.getElementsByName('matricula_professor')[0].value;
+  var senha = document.getElementsByName('senha')[0].value;
+
+  if (matricula_professor == "201501" && senha == "1234"){
+    alert("Dados corretos");
+    window.location.href= "../area-professor/index.html";
+  }else{
+    alert("Dados incorretos, tente novamente");
   }
+}
